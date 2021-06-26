ab7932fc71819e9216a3ebb2580c1b2bb12104f0
tarrows <rem_tate@yahoo.co.jp>
2021-06-26 23:56:55 +0900

    construct function

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -33,6 +33,10 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		return nativeBoolToBooleanObject(node.Value)
 	case *ast.Identifier:
 		return evalIdentifier(node, env)
+	case *ast.FunctionLiteral:
+		params := node.Parameters
+		body := node.Body
+		return &object.Function{Parameters: params, Env: env, Body: body}
 	case *ast.ReturnStatement:
 		val := Eval(node.ReturnValue, env)
 		if isError(val) {
